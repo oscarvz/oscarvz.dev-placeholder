5965d80242b3b1c88d1478b6c48e8bbd056c5ad2
Oscar van Zijverden <o.vanzijverden@gmail.com>
2021-01-14 12:53:13 +0100

    Refactor darkMode.js

--- src/js/darkMode.js
+++ src/js/darkMode.js
@@ -4,18 +4,21 @@ const button = document.querySelector('.theme-toggle');
 
 const newPreference = prefersDark ? 'light' : 'dark';
 
-const removeLocalStorage = () => window.localStorage.removeItem('theme');
-const saveToLocalStorage = (preference) => window.localStorage.setItem('theme', preference);
 const toggleBodyClass = (preference) => document.body.classList.toggle(`theme-${preference}`);
+const saveToLocalStorage = (preference) => window.localStorage.setItem('theme', preference);
+const removeFromLocalStorage = () => window.localStorage.removeItem('theme');
 
 const toggleMode = () => {
   toggleBodyClass(newPreference);
 
   if (document.body.classList.contains(`theme-${newPreference}`)) saveToLocalStorage(newPreference);
-  else removeLocalStorage();
+  else removeFromLocalStorage();
 };
 
 button.addEventListener('click', toggleMode);
 
-if (currentTheme === 'dark') prefersDark ? removeLocalStorage() : toggleBodyClass('dark');
-if (currentTheme === 'light') !prefersDark ? removeLocalStorage() : toggleBodyClass('light');
+if ((currentTheme === 'dark' && prefersDark) || (currentTheme === 'light' && !prefersDark)) {
+  removeFromLocalStorage();
+} else {
+  toggleBodyClass(currentTheme);
+}
