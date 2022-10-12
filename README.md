7c19f4f5132b13766ad1947bde944e2743f45c04
oscarvz <33732524+oscarvz@users.noreply.github.com>
2022-10-12 16:45:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Placeholder page for [oscarvz.dev](https://oscarvz.dev)
+
+Currently I'm working on a nice personal website but it's not finished yet.
+For now this simple placeholder has to do, but soon I'll deploy the new project.
+
+## Instructions
+
+To run this project locally, simply just:
+
+- Clone repo
+- Run `yarn` or `npm install` in your terminal
+- Run `yarn build` or `npm run build` in your terminal
+- Open `public/index.html`
+
+---
+
+Project is deployed on [Vercel](https://vercel.com).
+
+A special thanks to the contributors of [react-icons](https://github.com/react-icons/react-icons). I'm obviously not making use of React here, but the repo makes it easy to find & use SVG icons.
---
+++ src/js/darkMode.js
@@ -0,0 +1,25 @@
+// This dark mode implementation checks system-level preference, sets a new preference after
+// toggling and removes the saved preference in case of a changed/dynamic system-level setting
+const { matches: prefersDark } = window.matchMedia('(prefers-color-scheme: dark)');
+const savedThemePreference = localStorage.getItem('theme');
+const button = document.querySelector('.theme-toggle');
+
+const currentPreference = prefersDark ? 'dark' : 'light';
+const newPreference = prefersDark ? 'light' : 'dark';
+
+const toggleBodyClass = (preference) => document.body.classList.toggle(`theme-${preference}`);
+const saveToLocalStorage = (preference) => window.localStorage.setItem('theme', preference);
+const removeFromLocalStorage = () => window.localStorage.removeItem('theme');
+
+const toggleMode = () => {
+  toggleBodyClass(newPreference);
+
+  if (document.body.classList.contains(`theme-${newPreference}`)) saveToLocalStorage(newPreference);
+  else removeFromLocalStorage();
+};
+
+button.addEventListener('click', toggleMode);
+
+if (savedThemePreference && savedThemePreference !== currentPreference) {
+  toggleBodyClass(savedThemePreference);
+}
